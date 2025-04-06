ff80e97d05981d1edca8a6c5e38592f153bb09ba
Biwash Pradhan <biwashpra3883@gmail.com>
2025-04-06 08:59:32 +0545

    Feat/messages services (#2)
    * feat: (created) messages repository
    
    * feat: (created) messages service
    
    * chore: connected service to controller with bad code (witout DI)
    
    * chore: messages json to read and write messsages
    
    * feat: throw nest error NotFoundException
    
    * feat: (added) DI and module-providers
    
    * refact: messages file after DI
    
    * fixes: disabled eslint

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {
+    // service is creating its own repository dependencies. This is bad.
+    // DONOT DO THIS IN REAL APPS.
+    // Instead we use Nest Dependency Injection to inject the repository into the service.
+    // this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    return body;
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return { id };
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message;
   }
 }

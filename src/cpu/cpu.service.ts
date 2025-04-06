4726789f4954cb51a7811f075bd4d7591ea9a234
Biwash Pradhan <biwashpra3883@gmail.com>
2025-04-06 19:39:40 +0545

    feat: cpu and disk with imports and exports

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(public powerService: PowerService) {}
+
+  compute(num1: number, num2: number) {
+    const sum = num1 + num2;
+    const power = this.powerService.supplyPower(sum);
+    return `The sum of ${num1} and ${num2} is ${sum} | Power: ${power}`;
+  }
+}

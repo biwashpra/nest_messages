51936fce8c24f3d9038ef490c072629e10865137
Biwash Pradhan <biwashpra3883@gmail.com>
2025-04-06 08:15:20 +0545

    chore: connected service to controller with bad code (witout DI)

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // service is creating its own repository dependencies. This is bad.
+    // DONOT DO THIS IN REAL APPS.
+    // Instead we use Nest Dependency Injection to inject the repository into the service.
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    return body;
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    return { id };
+    return this.messagesService.findOne(id);
   }
 }
